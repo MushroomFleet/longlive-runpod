1e83e81724562ff12f567ceb61e194aeccd171fb
yukang <yukangchen@cse.cuhk.edu.hk>
2025-11-04 00:17:10 +0800

    Add files via upload

--- utils/lora_utils.py
+++ utils/lora_utils.py
@@ -2,7 +2,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
-# To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
+# To view a copy of this license, visit http://www.apache.org/licenses/LICENSE-2.0
 #
 # No warranties are given. The work is provided "AS IS", without warranty of any kind, express or implied.
 #
